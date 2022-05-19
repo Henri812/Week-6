c7420fdfba4576c50ff6961de498d24997474b9d
Henri Heinmäe <henriheinmae@gmail.com>
2022-05-19 21:50:38 +0300

    add new projects

---
+++ StringReplace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
